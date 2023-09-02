e489be1c10dc8a9dd19b3446f6f99ee9f758c86e
kostas230 <57586901+kostas230@users.noreply.github.com>
2023-09-02 12:03:03 +0300

    Update install_apps.sh

--- linux/install_apps.sh
+++ linux/install_apps.sh
@@ -3,8 +3,8 @@ pacman -S linux-zen linux-zen-headers xterm pulseaudio pulseaudio-alsa gnome-the
 openssh iwd openresolv wpa_supplicant wireless_tools dialog lvm2 grub efibootmgr papirus-icon-theme \
 dosfstools mtools nano zsh os-prober wget curl git apparmor unrar unzip cups vim xsane lxappearance \
 sane-gt68xx-firmware ranger evince w3m xf86-video-intel libva-intel-driver tint2 xfce4-screenshooter \
-mesa mpv vlc libreoffice chromium geeqie gimp electron telegram-desktop transmission-gtk \
-xterm xscreensaver mutt feh xorg xorg-server xorg-apps xorg-xinit xorg-xclock \
+mesa mpv libreoffice chromium geeqie gimp electron telegram-desktop transmission-gtk \
+xscreensaver mutt feh xorg xorg-server xorg-apps xorg-xinit xorg-xclock \
 terminus-font ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid \
 gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts \
 ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code \
