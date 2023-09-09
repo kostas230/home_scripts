7c1baf2c09906716ca7af385b1c0f0b91e3ba629
Kostas <kostas230@lenovo-laptop.my.domain>
2023-09-09 20:50:33 +0300

    updated some config files

--- openbsd/config_files/install_configs.sh
+++ openbsd/config_files/install_configs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cp -v xorg.conf /etc/X11/ 
-cp -v XSetup_0 /etc/X11/xenodm/
-cp -v Xresources /etc/X11/xenodm/
+cp -v /home/kostas230/.config/home_scripts/openbsd/config_files/xorg.conf /etc/X11/ 
+cp -v /home/kostas230/.config/home_scripts/openbsd/config_files/XSetup_0 /etc/X11/xenodm/
+cp -v /home/kostas230/.config/home_scripts/openbsd/config_files/Xresources /etc/X11/xenodm/
--- openbsd/install_symlinks.sh
+++ openbsd/install_symlinks.sh
@@ -4,6 +4,6 @@ ln -sf ~/.config/home_scripts/openbsd/Xresources ~/.Xresources
 ln -sf ~/.config/home_scripts/openbsd/cwmrc ~/.cwmrc
 ln -sf ~/.config/home_scripts/openbsd/robbyrussell-new.zsh-theme ~/.oh-my-zsh/themes/robbyrussell-new.zsh-theme
 ln -sf ~/.config/home_scripts/openbsd/xinitrc ~/.xinitrc
-ln -sf ~/.config/home_scripts/openbsd/xinitrc ~/.session
+ln -sf ~/.config/home_scripts/openbsd/xinitrc ~/.xsession
 
 #ranger --copy-config=rc
