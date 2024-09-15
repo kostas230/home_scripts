120145c7a264c5f5120864133b767aa31ff29fee
kostas230 <57586901+kostas230@users.noreply.github.com>
2024-09-15 23:33:12 +0100

    Update set_symlinks.sh

--- linux/set_symlinks.sh
+++ linux/set_symlinks.sh
@@ -1,10 +1,11 @@
-#!/bin/zsh
-ln -sf ~/.config/home_scripts/linux/xinitrc ~/.xinitrc
-ln -sf ~/.config/home_scripts/linux/xinitrc ~/.xsession
-ln -sf ~/.config/home_scripts/linux/zprofile ~/.zprofile
-ln -sf ~/.config/home_scripts/linux/Xresources ~/.Xresources
-ln -sf ~/.config/home_scripts/linux/cwmrc ~/.cwmrc
-ln -sf ~/.config/home_scripts/linux/vimrc ~/.vimrc
-cp -vfr ~/.config/home_scripts/linux/robbyrussell.zsh-theme ~/.oh-my-zsh/themes/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#!/bin/sh
+
+ln -sf $HOME/.config/home_scripts/linux/xinitrc $HOME/.xinitrc
+ln -sf $HOME/.config/home_scripts/linux/xinitrc $HOME/.xsession
+ln -sf $HOME/.config/home_scripts/linux/zprofile $HOME/.zprofile
+ln -sf $HOME/.config/home_scripts/linux/Xresources $HOME/.Xresources
+ln -sf $HOME/.config/home_scripts/linux/cwmrc $HOME/.cwmrc
+ln -sf $HOME/.config/home_scripts/linux/vimrc $HOME/.vimrc
+cp -vfr $HOME/.config/home_scripts/linux/robbyrussell.zsh-theme $HOME/.oh-my-zsh/themes/
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
