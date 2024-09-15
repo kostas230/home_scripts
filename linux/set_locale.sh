d6615af9bd635ce992d3a089736654ec20913d0b
kostas230 <57586901+kostas230@users.noreply.github.com>
2024-09-15 23:31:37 +0100

    Update set_locale.sh

--- linux/set_locale.sh
+++ linux/set_locale.sh
@@ -1,12 +1,23 @@
 #!/bin/zsh
 # Must be run from root acc
-echo "en_US.UTF-8 UTF-8" | tee -a /etc/locale.gen
+
+# Locale setup
+echo "KEYMAP=uk" | tee -a /etc/vconsole.conf
+echo "en_GB.UTF-8 UTF-8" | tee -a /etc/locale.gen
 echo "el_GR.UTF-8 UTF-8" | tee -a /etc/locale.gen
-echo "LANG=en_US.UTF-8" | tee -a /etc/locale.conf 
+echo "LANG=en_GB.UTF-8" | tee -a /etc/locale.conf 
+echo "LC_COLLATE=C" | tee -a /etc/locale.conf 
 locale-gen
-ln -sf /usr/share/zoneinfo/Europe/Athens /etc/localtime 
-hwclock --systohc --utc 
+
+# Set up timezone
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime 
+hwclock --systohc --utc
+
+# hosts configuration
 echo "lenovo-laptop" | tee -a /etc/hostname 
 echo "127.0.0.1 localhost" | tee -a /etc/hosts
 echo "::1 localhost" | tee -a /etc/hosts
-echo "127.0.1.1 lenovo-laptop" | tee -a /etc/hosts  
+echo "127.0.1.1 lenovo-laptop" | tee -a /etc/hosts
+
+# Default editor is Vim
+echo "export EDITOR=vim"
