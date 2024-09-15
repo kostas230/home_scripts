ecd861473cb51ad8029e8f28b4782faa80c0ba52
kostas230 <57586901+kostas230@users.noreply.github.com>
2024-09-15 23:25:18 +0100

    Update upgrade_aur.sh

--- linux/upgrade_aur.sh
+++ linux/upgrade_aur.sh
@@ -1,45 +1,52 @@
 #!/bin/sh
-mkdir -p ~/.src
-cd ~/.src
 
-git clone https://aur.archlinux.org/cwm.git
-git clone https://aur.archlinux.org/skypeforlinux-stable-bin.git
-git clone https://aur.archlinux.org/nordvpn-bin.git
-git clone https://aur.archlinux.org/tor-browser.git 
+mkdir $HOME/.src
+cd $/HOME/.src
+
+# Ungoogled Chromium
+git clone https://aur.archlinux.org/ungoogled-chromium-bin.git
+cd ungoogled-chromium-bin
+makepkg -si
+cd ..
+
+# 1password 
+git clone https://aur.archlinux.org/1password.git
+cd 1password
+makepkg -si
+cd ..
+
+# Mullvad VPN
+curl -sS https://mullvad.net/media/mullvad-code-signing.asc | gpg --import
+git clone https://aur.archlinux.org/mullvad-vpn-bin.git
+cd mullvad-vpn-bin
+makepkg -si
+cd ..
+
+# EinsteinPy
+git clone https://aur.archlinux.org/python-einsteinpy_geodesics.git
+cd python-einsteinpy_geodesics
+makepkg -si
+cd ..
+
+git clone https://aur.archlinux.org/python-einsteinpy.git
+cd python-einsteinpy
+makepkg -si
+cd ..
+
+git clone https://aur.archlinux.org/python-einsteinpy-doc.git
+cd python-einsteinpy-doc
+makepkg -si
+cd ..
+
+# WxMaxima 
+gpg --recv-keys 5C86C0E4211D5B8E
+git clone https://aur.archlinux.org/wxmaxima.git
+cd wxmaxima
+makepkg -si
+cd ..
+
+# Zoom
 git clone https://aur.archlinux.org/zoom.git
-git clone https://aur.archlinux.org/nvidia-470xx-utils.git
-git clone https://aur.archlinux.org/lib32-nvidia-470xx-utils.git
-git clone https://aur.archlinux.org/python-vdf.git
-git clone https://aur.archlinux.org/protontricks.git
-
-gpg --recv-keys E9DE70742CFE5BF9
-cd ~/.src/cwm
+cd zoom
 makepkg -si
-
-cd ~/.src/skypeforlinux-stable-bin
-makepkg -si
-
-cd ~/.src/nordvpn-bin
-makepkg -si
-
-gpg --auto-key-locate nodefault,wkd --locate-keys torbrowser@torproject.org 
-cd ~/.src/tor-browser
-makepkg -si 
-
-cd ~/.src/zoom
-makepkg -si 
-
-cd ~/.src/nvidia-470xx-utils
-makepkg -si
-
-cd ~/.src/lib32-nvidia-470xx-utils
-makepkg -si
-
-cd ~/.src/python-vdf
-makepkg -si
-
-cd ~/.src/protontricks
-makepkg -si 
-
-cd
-rm -rf ~/.src/*
+cd ..
