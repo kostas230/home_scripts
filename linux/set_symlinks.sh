c8c3736ddf694c0cd3fd7427b09edf95c854773f
kostas230 <57586901+kostas230@users.noreply.github.com>
2025-02-11 22:20:57 +0000

    Update set_symlinks.sh

--- linux/set_symlinks.sh
+++ linux/set_symlinks.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-ln -sf $HOME/.config/home_scripts/linux/xinitrc $HOME/.xinitrc
-ln -sf $HOME/.config/home_scripts/linux/xinitrc $HOME/.xsession
-ln -sf $HOME/.config/home_scripts/linux/zprofile $HOME/.zprofile
-ln -sf $HOME/.config/home_scripts/linux/Xresources $HOME/.Xresources
-ln -sf $HOME/.config/home_scripts/linux/cwmrc $HOME/.cwmrc
-ln -sf $HOME/.config/home_scripts/linux/vimrc $HOME/.vimrc
-cp -vfr $HOME/.config/home_scripts/linux/robbyrussell.zsh-theme $HOME/.oh-my-zsh/themes/
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
